a7a399f320eba0df6569e5c2cda3fd0f8c20be1e
HZ <hzeng1998@gmail.com>
2017-02-28 19:35:18 +0800

    remote

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<cstdio>
+
+using namespace std;
+
+int main()
+{
+	int st[10] = {73, 76, 79, 86, 69, 89, 79, 85, 33};
+	for (int i = 0; i <= 8; i++){
+		printf("%c ", st[i]);
+	}
+	return 0;
+}
